c097e89d143e3963b2afc4426278f209de63d789
PREDATOR\chair <chairadr@gmail.com>
2025-06-16 19:38:17 -0300

    mais testes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
-alert("oie");
+function soma(valor1,valor2){
+return valor1+valor2;
+}
+//////
+document.getElementById("texto").innerHTML = soma(10,10);
+
+//////
+function alertahello(){
+alert("ai meu Deus");
+}
+/////
+function paracelcius(valorfar){
+    var x = (5 / 9) * (valorfar - 32);
+
+    alert("A temperatura é de " + x + " graus celcius");
+}
+
+const carro = {
+
+marca: "ford",
+modelo:"ka",
+ano:2015,
+placa:"abc-1234",
+buzinha: function (){ alert('Biiiii')}
+
+}
+
+document.getElementById("saida").innerHTML = 
+  "Marca: " + carro.marca + "<br>" +
+  "Modelo: " + carro.modelo + "<br>" +
+  "Ano: " + carro.ano + "<br>" +
+  "Placa: " + carro.placa;
+
+//document.getElementById("saida").innerHTML = carro();
+
+
